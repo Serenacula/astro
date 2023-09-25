0052dd78cc51053f7411d2d2549f2ebb8ce4cd6e
Serenacula <faylee@hanginggardens.com>
2023-09-25 23:11:11 +0100

    moving react-icons into src

---
+++ src/components/social/lib/esm/iconsManifest.js
@@ -0,0 +1,9 @@
+export var IconsManifest = [
+    {
+        "id": "bs",
+        "name": "Bootstrap Icons",
+        "projectUrl": "https://github.com/twbs/icons",
+        "license": "MIT",
+        "licenseUrl": "https://opensource.org/licenses/MIT",
+    },
+]
