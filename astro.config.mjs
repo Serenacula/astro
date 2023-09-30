571910328bbef921d78eab9ed33cc91ef1044df1
Serenacula <faylee@hanginggardens.com>
2023-09-30 12:06:03 +0100

    site config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from "astro/config"
 
-import react from "@astrojs/react";
+import react from "@astrojs/react"
 
 // https://astro.build/config
 export default defineConfig({
-  experimental: {
-    assets: true
-  },
-  integrations: [react()]
-});
+    experimental: {
+        assets: true,
+    },
+    integrations: [react()],
+    site: "https://serenacula.github.io",
+    base: "/astro",
+})
